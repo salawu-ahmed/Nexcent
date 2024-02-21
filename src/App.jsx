7380e9cbb62ada79330a2eccaa2c832db6dcd26f
ahmed <a.salawu@outlook.com>
2024-02-21 17:46:44 +0000

    Update: Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>Hello World, Welcome to my agency website</h1>
+        <p>This is just the basic setup of the website. Enjoy!</p>
+    </>
+  )
+}
+
+export default App
